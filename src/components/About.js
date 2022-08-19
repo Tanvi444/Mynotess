f78d1fba8a81461a72776d4c893856abc66ec4c8
Tanvi <tanvitomar592@gmail.com>
2022-08-20 03:01:59 +0530

    first

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+// // 
+// import React, { useContext, useEffect } from 'react'
+
+// const About = () => { 
+//     return (
+//         <div>
+//             This is About page
+//         </div>
+//     )
+// }
+
+// export default About
+import React from 'react'
+
+const About = () => { 
+    return (
+        <div>
+            This is About page
+        </div>
+    )
+}
+
+export default About
+//hiuguitg
