d9e722f99e16f636a43da034f6142aba441fd70f
Tanvi <tanvitomar592@gmail.com>
2022-08-20 03:12:38 +0530

    first1

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,7 +6,7 @@ const Signup = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const {name,email,password,cpassword}=credentials;
-        const response = await fetch("http://localhost:5000/api/auth/createUser", {
+        const response = await fetch("https://mynoteess.herokuapp.com/api/auth/createUser", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
